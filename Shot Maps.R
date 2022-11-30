ae219e7b7c6247972688c9354a15b6ee09a54a17
Julian B <76627515+jbr2020@users.noreply.github.com>
2022-11-30 16:04:13 +0100

    Update Shot Maps.R

--- Shot Maps.R
+++ Shot Maps.R
@@ -1,9 +1,15 @@
+#A shot map using RStudio for Mo Salah´s shots in the 2019/20 season
+
 library(ggplot2)
 library(png)
 
 Shots = read.csv("ShotLocations (2).csv")
 Salah = readPNG("Salah.png")
 shotcolours = c("goal" ="blue", "missed" = "green4", "saved" = "yellow")
+
+#A half-pitch is plotted before seperating shots into outcome (goal, missed, saved) by colour and body part (head, left, right) by shape
+
+#Includes a profile picture of Mo Salah and his total xG for the illustrated shots
 
 paste("xG:", sum(Shots$xG))
 
@@ -36,3 +42,7 @@ ggplot(Shots, aes(x =locx, y = locy)) +
   annotate("text", label = "Mo Salah - 19/20", x=0, y=70) +
   annotate("text", label = paste("xG:", sum(Shots$xG)), x=0, y=65) +
   annotation_raster(Salah, xmin=25, xmax= 35, y=65, ymax= 75)
+
+#The map shows that when shooting from outside of the area, Mo Salah tends to shoot from the right side of the pitch but with his left foot. 
+
+#Analysts can use this information to draw the conclusion that he likes to "cut inside" from the right wing onto his stronger foot and therefore oppositions defenders should try to force him wide. 
