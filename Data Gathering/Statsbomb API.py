fa8f7dac2d2f43f00194b0b2888a057c46d1902d
Julian B <76627515+JulianB22@users.noreply.github.com>
2025-04-01 22:17:12 +0200

    Update Statsbomb API.py

--- Data Gathering/Statsbomb API.py
+++ Data Gathering/Statsbomb API.py
@@ -28,3 +28,16 @@ df = pd.concat(all_matches)
 Bonmati = df[df['player'] == 'Aitana Bonmati Conca']
 
 Bonmati.head()
+
+
+#Summary of StatsBomb functions:
+#load() or load_data(): Loads the primary match, event, and team data.
+#competitions(): Retrieves competition data.
+#matches(): Retrieves match-level details.
+#events(): Retrieves event data for matches.
+#teams(): Retrieves team-level data.
+#players(): Retrieves player-related data for matches.
+#games(): Retrieves match data for a specific competition and season.
+#event_types(): Retrieves available event types.
+#players_per_match(): Retrieves player data for a specific match.
+#get_match_data(): Retrieves comprehensive match data.
